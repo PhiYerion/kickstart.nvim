e9947f4342056c564156403650c20626607dee78
phiyerion <reidbandy@gmail.com>
2023-11-06 15:59:29 -0500

    characters for space and tab

--- init.lua
+++ init.lua
@@ -39,6 +39,9 @@ P.S. You can delete this when you're done too. It's your config now :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.opt.list = true
+vim.opt.listchars:append({ tab = '▶ ', space = '·', trail = '•', nbsp = '•', })
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.wo.relativenumber = true
