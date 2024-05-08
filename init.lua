dd1ce229d1d09c8bc4b4f04881a334af02c62aae
Reid Bandy <reidbandy@gmail.com>
2024-05-08 12:55:12 -0400

    load opts file

--- init.lua
+++ init.lua
@@ -881,7 +881,7 @@ require('lazy').setup({
   --
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
@@ -914,5 +914,6 @@ require('lazy').setup({
   },
 })
 
+require 'opts'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
